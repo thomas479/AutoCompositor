64096d5c8fba97a1fdefa1bef9750ba396bd84e1
Patrick <patrickstijf1@gmail.com>
2016-09-29 12:22:09 +0200

    Implementation Auto Class

--- AutoCompositor/Compositor/Auto.cs
+++ AutoCompositor/Compositor/Auto.cs
@@ -9,6 +9,15 @@ namespace AutoCompositor.Compositor
 {
     class Auto : IAutoComponent
     {
+        public int xPosition { get; set; }
+        public int yPosition { get; set; }
+        public Color color { get; set; }
+
+        public Auto()
+        {
+            color = Color.BurlyWood;
+        }
+
         public void Add(IAutoComponent autoComponent)
         {
             throw new NotImplementedException();
@@ -21,7 +30,8 @@ namespace AutoCompositor.Compositor
 
         public void Move(int x, int y)
         {
-            //Do move logic
+            xPosition = x;
+            yPosition = y;
         }
 
         public void Remove(IAutoComponent autoComponent)
